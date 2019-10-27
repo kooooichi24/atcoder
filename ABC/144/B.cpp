ff26c4d11013ec85985e83aaa8889e34a4b17499
古川広一 <ex.kouichi1224@gmail.com>
2019-10-27 22:30:57 +0900

    ABC/144/A-C

---
+++ ABC/144/B.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+typedef long long ll;
+
+int main() {
+  int N;
+  cin >> N;
+
+  string ans = "No";
+  for (int i = 1; i < 10; i++){
+    for (int j = 1; j < 10; j++){
+      if (N == i*j) ans = "Yes";
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
